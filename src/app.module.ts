a900b173967e830e31a97028859361c2558aff39
Ahmed Sayed <therealahme@gmail.com>
2024-02-22 12:36:06 +0200

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ExcelModule } from './excel/excel.module';
+import { BookModule } from './books/books.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_LOCAL_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ExcelModule,
+    BookModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
